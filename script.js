037a027c1b5e63ecd13113ee2df31bee7132e10a
Your Name <you@example.com>
2021-10-15 10:57:36 +0700

    p

--- script.js
+++ script.js
@@ -6,20 +6,72 @@ $(document).ready(function(){
 var privates = document.querySelectorAll(".onchat-private");
 privates.forEach(private =>{
     private.onclick = function(){
-        document.querySelector(".list-chat.active").classList.remove("active");
-        document.querySelector(".chat-private").classList.add("active");
+        document.querySelector(".list-chat").style.display = "none";
+        document.querySelector(".chat-private").style.display = "block";
     }
 })
 
+function backToMeunu(){           
+    document.querySelector(".list-chat").style.display = "block";;
+    document.querySelector(".chat-private").style.display = "none";;
+}
+function closeModalGroup(){
+    document.querySelector(".modal-group").style.display = "none";
+}
+function showModalGroup(){
+    document.querySelector(".modal-group").style.display = "block";
+}
+function showModalSent(){
+    document.querySelector(".modal-sent").style.display = "block";
+}
+function closeModalSent(){
+    document.querySelector(".modal-sent").style.display = "none";
+}
+
+var selects = document.querySelectorAll(".select-user.active");
+const afterSelects = document.querySelectorAll(".after-select");
+selects.forEach((select,index) =>{
+    const afterSelect = afterSelects[index];
+    select.onclick = function(){
+        select.style.display ="none";
+        afterSelect.style.display ="block";
+    }
+})
+
+var closes = document.querySelectorAll(".close-select");
+closes.forEach((close,index) =>{
+    const select = selects[index];
+    const afterSelect = afterSelects[index];
+    close.onclick = function(){
+        afterSelect.style.display ="none";
+        select.style.display = "block";
+    }
+})
+var selectSents = document.querySelectorAll(".sent-user");
+const afterSents = document.querySelectorAll(".after-sent");
+selectSents.forEach((selectSent,index) =>{
+    const afterSent = afterSents[index];
+    selectSent.onclick = function(){
+        selectSent.style.display ="none";
+        afterSent.style.display ="block";
+    }
+})
 
 const signUpButton = document.getElementById('signUp');
-        const signInButton = document.getElementById('signIn');
-        const container = document.getElementById('container');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
-        signUpButton.addEventListener('click', () => {
-            container.classList.add('right-panel-active');
-        });
+signUpButton.addEventListener('click', () => {
+    container.classList.add('right-panel-active');
+});
 
-        signInButton.addEventListener('click', () => {
-            container.classList.remove('right-panel-active');
-        });
+signInButton.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
+});
+
+
+
+
+
+
+
